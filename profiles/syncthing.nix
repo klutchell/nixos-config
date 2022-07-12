a043d60c61b05eaaa69ed71dfe6daf442fcb030e
Kyle Harding <kyle@balena.io>
2022-07-12 10:10:13 -0400

    Open ports for syncthing
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- profiles/syncthing.nix
+++ profiles/syncthing.nix
@@ -8,4 +8,9 @@ with lib;
     dataDir = "/home/kyle/.syncthing";    # Default folder for new synced folders
     configDir = "/home/kyle/.config/syncthing";   # Folder for Syncthing's settings and keys
   };
+
+  # Syncthing ports
+  # https://docs.syncthing.net/users/firewall.html
+  networking.firewall.allowedTCPPorts = [ 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }
