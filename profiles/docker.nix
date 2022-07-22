e2c9c6eb86d31b365dbb7234f98086624e8b34f0
Kyle Harding <kyle@balena.io>
2022-07-22 12:59:53 -0400

    Install skopeo in docker profile
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- profiles/docker.nix
+++ profiles/docker.nix
@@ -4,4 +4,8 @@ with lib;
   # https://nixos.wiki/wiki/Docker
   # https://search.nixos.org/options?channel=22.05&from=0&size=50&sort=relevance&type=packages&query=virtualisation.docker
   virtualisation.docker.enable = true;
+
+  environment.systemPackages = [
+      pkgs.skopeo
+  ];
 }
