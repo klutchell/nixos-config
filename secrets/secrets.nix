9892787ed74ec11206ab5c85eda49220bbbc9470
Kyle Harding <kyle@balena.io>
2022-09-01 21:15:08 -0400

    Update secret keys
    Signed-off-by: Kyle Harding <kyle@balena.io>

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,13 @@
 # https://github.com/divnix/digga/blob/main/doc/secrets.md
+# https://github.com/ryantm/agenix
 let
   # set ssh public keys here for your systems and users
   user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICzLUZyHJY2j/hIwnCPdn3yRvUyYYXYnMf+E3m3Dz9Wd kyle@jupiter";
-  user2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILI6jSq53F/3hEmSs+oq9L4TwOo1PrDMAgcA1uo1CCV/";
+  user2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPCU5BdTv0V2DxRml0Dgab1EVayR5ydkQSfeORZmVyKx kyle@neptune";
   users = [ user1 user2 ];
 
   system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdmHg6mPdyKegPkWekdYKwEER8qgHjFNqx5Sgqf3YbT root@jupiter";
-  system2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzxQgondgEYcLpcPdJLrTdNgZ2gznOHCAxMdaceTUT1";
+  system2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJtECRE3g5aI6dcZjPVpP/FptgGATKHUPIOHdYouZ4iL root@neptune";
   systems = [ system1 system2 ];
 in
 {
